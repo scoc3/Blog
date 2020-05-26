f1b130e541e9207c997273b9c3f65df2e36cbba6
scoc3 <scoc3@outlook.com>
2020-05-26 19:19:55 +0100

    My first Django app, first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\visual_studio\code\python\django\django\django\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
